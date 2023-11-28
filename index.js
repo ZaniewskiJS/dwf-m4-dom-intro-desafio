b05a0592b889d681afebade078c245571a46f68d
Gonzalo Zaniewski <gonzalozaniewski@outlook.com>
2023-11-28 13:15:43 -0300

    ejercio realizados

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-const cosasQueAprendimos = [
-  {
-    tema: "terminal",
-    class: "",
-  },
-  {
-    tema: "node",
-    class: "",
-  },
-  {
-    tema: "oop",
-    class: "",
-  },
-  {
-    tema: "typescript",
-    class: "",
-  },
-  {
-    tema: "css",
-    class: "",
-  },
-  {
-    tema: "dom",
-    class: "special",
-  },
-];
+document.addEventListener('DOMContentLoaded', function () {
+  const cosasQueAprendimos = [
+    { tema: "terminal", class: "" },
+    { tema: "node", class: "" },
+    { tema: "oop", class: "" },
+    { tema: "typescript", class: "" },
+    { tema: "css", class: "" },
+    { tema: "dom", class: "special" },
+  ];
 
-function main() {}
+  function main() {
+    const listaUl = document.getElementById('lista');
+    listaUl.innerHTML = '';
 
-main();
+    cosasQueAprendimos.forEach(item => {
+      const nuevoLi = document.createElement('li');
+      nuevoLi.textContent = item.tema;
+
+      if (item.class) {
+        nuevoLi.classList.add(item.class);
+      }
+
+      listaUl.appendChild(nuevoLi);
+    });
+  }
+
+  // Llamar a la función main al cargar el DOM
+  main();
+});
